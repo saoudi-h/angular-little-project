07935f0774d7593e35c6f88c2f6c40183b3ed2b6
Hakim Saoudi <saoudihakim@gmai.com>
2023-07-26 17:14:48 +0200

    recipe up

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe("A Test Recipe","This is simply a test","https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2428&q=80")
+  ];
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }

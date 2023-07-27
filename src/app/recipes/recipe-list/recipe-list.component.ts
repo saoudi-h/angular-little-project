85ec308fb79d6505d56d357d1dc234e023d26da2
Hakim Saoudi <saoudihakim@gmai.com>
2023-07-27 10:04:14 +0200

    up recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -18,6 +18,11 @@ export class RecipeListComponent implements OnInit {
       'a new recipe',
       'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2428&q=80'
     ),
+    new Recipe(
+      'taboulet',
+      'a new recipe',
+      'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2428&q=80'
+    ),
   ];
 
   ngOnInit(): void {

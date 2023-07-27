6ace679e11bd820bab04e74e5bd6a1ed5359b6ae
Hakim Saoudi <saoudihakim@gmai.com>
2023-07-27 11:04:03 +0200

    add ingredients + FIX OnInit throw exception(vs code)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -5,7 +5,7 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent {
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
@@ -24,8 +24,4 @@ export class RecipeListComponent implements OnInit {
       'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2428&q=80'
     ),
   ];
-
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
-
+export class ShoppingListComponent{
+  ingredients:Ingredient[] = [
+    new Ingredient("Sucre",500),
+    new Ingredient("Ananas",2),
+    new Ingredient("chocolat",3)
+  ];
 }
